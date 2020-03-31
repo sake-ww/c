902fff037b616f7d908a4352d9c755df992458f7
sake-ww <875805569@qq.com>
2020-03-31 17:16:53 +0800

    二月廿七日代吗

---
+++ test-2-27/test-2-27/Test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c, max;
+	printf("please input a,b,c:\n");
+	scanf("%d,%d,%d", &a, &b, &c);
+	max = a;
+	if (max<b)
+		max = b;
+	if (max<c)
+		max = c;
+	printf("The largest number is %d\n", max);
+	return 0;
+}
+
+/*
+int main()
+{
+	printf("hello word\n");
+	return 0;
+}
+*/
