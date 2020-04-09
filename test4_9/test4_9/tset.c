f164731b6c00b13b41e8ba45b2c21d589d812980
sake-ww <875805569@qq.com>
2020-04-09 20:41:51 +0800

    上课内容

---
+++ test4_9/test4_9/tset.c
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	for (int i = 3; i < 100; i += 3)
+//	{
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	for (int i = 7; i < 100; i++)
+//	{
+//		if (i % 7 == 0)
+//		{
+//			printf("%d ", i);
+//		}
+//		if (i % 10 == 7)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	while (i < 100)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 1;
+//	int j = 1;
+//	while (i < 34)
+//	{
+//		j = i * 3;
+//		i++;
+//		printf("%d ", j);
+//	}
+//	return 0;
+//}
+
+//# include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		printf("%d\n", i);
+//		i = i + 3;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 1;
+//	int a = 1;
+//	while (i < 100)
+//	{
+//		if (i % 7 == 0 || i % 10 == 7)
+//		{
+//			printf("%d ", i);
+//		}
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i % 7 == 0 || i % 10 == 7)
+//		{
+//	    	printf("%d ", i);
+//		}
+//		i++;
+//	} while (i < 100);
+//	return 0;
+//}
+
+
+
