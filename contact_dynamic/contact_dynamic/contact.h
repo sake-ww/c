5200daf493af0985d754ba76700f76732b502f85
王为 <875805569@qq.com>
2020-05-08 16:20:41 +0800

    动态通讯录

---
+++ contact_dynamic/contact_dynamic/contact.h
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+//#define MAX 1000
+
+#define DEFAULT_SZ 3
+
+//写在这里是因为方便以后修改内容
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW
+	//SORT
+};
+
+typedef struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+
+//通讯录类型
+typedef struct Contact
+{
+	struct PeoInfo *data;
+	int size;
+	int capacity;//当前通讯录的最大容量
+}Contact;
+
+//声明函数
+//初始化通讯录的函数
+void InitContact(struct Contact* ps);
+
+//增加一个信息到通讯录
+void AddContact(struct Contact* ps);
+
+//打印通讯录中的信息
+void ShowContact(const struct Contact* ps);
+
+//删除指定的联系人
+void DelContact(struct Contact* ps);
+
+//查找指定的人的信息
+void SearchContct(const struct Contact* ps);
+
+//修改指定联系人
+void ModfyContact(struct Contact* ps);
+
+void DestroyContact(Contact* ps);
