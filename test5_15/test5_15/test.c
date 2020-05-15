7e7577ab8e47b23b9658a5d5a68ffc8ad9800efb
王为 <875805569@qq.com>
2020-05-15 16:39:45 +0800

    作业整理

---
+++ test5_15/test5_15/test.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void double_fun(double x)
+//{
+//	double b = 0;
+//	if (x > 3)
+//	{
+//		b = ((x*x) + 2);
+//	}
+//	else 
+//	{
+//		b = ((3 * x) + (7 * x*x));
+//	}
+//	printf("%lf", b);
+//	return 0;
+//}
+//int main()
+//{
+//	double x = 0;
+//	scanf("%lf", &x);
+//    double_fun(x);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a[3][3], sum = 0;
+//	int i, j;
+//	printf("输入一组3*3矩阵数据：");
+//	for (i = 0; i < 3; i++)
+//	for (j = 0; j < 3; j++)
+//		scanf("%d", &a[i][j]);//输入矩阵的值 
+//	printf("输出对角线数值之和：");
+//	for (i = 0; i < 3; i++)
+//		sum += a[i][i];//对角线求和 
+//	printf("%d", sum);
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1, n;
+//	int j = 1;
+//	scanf("%d",&n);
+//	while (i <= n)
+//	{
+//		j = j*i;
+//		i++;
+//	}
+//	printf("n!= %d\n",j);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
