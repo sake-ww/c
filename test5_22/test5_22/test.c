2345241f5314801a05c2f33680a8711fb1aeebe0
王为 <875805569@qq.com>
2020-05-22 19:44:00 +0800

    练习题

---
+++ test5_22/test5_22/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//报告1
+//第一题
+int main()
+{
+	printf("hello word\n");
+    return 0;
+}
+//第二题
+//int main()
+//{
+//	int a, b, c, max;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a > b)
+//		max = a;
+//	else
+//		max = b;
+//	if (max < c)
+//		max = c;
+//	printf("%d", max);
+//	return 0;
+//}
