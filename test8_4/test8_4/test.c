5ba9136ead29a66b39c0dad2ede7f31f60fe1d1f
王为 <875805569@qq.com>
2020-08-04 16:55:58 +0800

    将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给的那个的两个链表的所有节点组成的

---
+++ test8_4/test8_4/test.c
@@ -0,0 +1,93 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//5. 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//示例：
+//输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+
+//方法：将链表1和链表2中的每一个元素依次进行比较，将小的元素依次往新链表中进行尾插
+//typedef struct ListNode listNode;
+//struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+//{
+//	if (NULL == l1)
+//		return l2;
+//	if (NULL == l2)
+//		return l1;
+//
+//	//l1和l2均不为空
+//	ListNode* newhead = NULL;
+//	ListNode* cur1 = l1;
+//	ListNode* cur2 = l2;
+//
+//	if (cur1->val <= cur2->val)//--------1
+//	{
+//		newhead = cur1;
+//		tailNode = cur1;
+//		cur1 = cur->next;
+//	}
+//	else
+//	{
+//		newhead = cur2;
+//		tailNode = cur2;
+//		cur2 = cur2->next;
+//	}
+//	while (cur1 && cur2)//----------2
+//	{
+//		if (cur1->val1 <= cur2->val)
+//		{
+//			tailNode->next = cur1;
+//			cur1 = cur1->next;
+//		}
+//		else
+//		{
+//			tailNode->next = cur2;
+//			cur2 = cur2->next;
+//		}
+//		tailNode = tailNode->next;
+//	}
+//	if (cur1)
+//		tailNode->next = cur1;
+//	else
+//		tailNode->next = cur2;
+//
+//	return newhead;
+//}
+
+//1和2 里面有些内容有点重复
+
+//修改一下
+typedef struct ListNode listNode;
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+{
+	if (NULL == l1)
+		return l2;
+	if (NULL == l2)
+		return l1;
+
+	//l1和l2均不为空
+	ListNode* cur1 = l1;
+	ListNode* cur2 = l2;
+	ListNode newhead;
+	ListNode* tailNode = &newhead;
+	
+	while (cur1 && cur2)
+	{
+		if (cur1->val1 <= cur2->val)
+		{
+			tailNode->next = cur1;
+			cur1 = cur1->next;
+		}
+		else
+		{
+			tailNode->next = cur2;
+			cur2 = cur2->next;
+		}
+		tailNode = tailNode->next;
+	}
+	if (cur1)
+		tailNode->next = cur1;
+	else
+		tailNode->next = cur2;
+
+	return newhead.next;
+}
